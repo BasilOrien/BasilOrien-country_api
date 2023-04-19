7e53cbb7590a2aadb31d0c9b24d19f11d78551ca
BasilOrien <cuentaparaestudiosf@gmail.com>
2023-04-19 16:36:49 -0300

    cerceljsonv3!

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://country-client-jlv7u3zzs-basilorien.vercel.app/"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://country-client-jlv7u3zzs-basilorien.vercel.app/"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
