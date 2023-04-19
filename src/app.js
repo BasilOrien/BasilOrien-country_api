254e0224120b5745afedea40a9d326a83082ffbc
BasilOrien <cuentaparaestudiosf@gmail.com>
2023-04-19 15:53:24 -0300

    algo

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://country-client-jlv7u3zzs-basilorien.vercel.app/"
+    "https://country-client.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
